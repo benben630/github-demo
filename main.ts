75ba46922c8e9841586ca58e94aedc048db3fdaa
benben630 <nguyen012014@gmail.com>
2025-03-20 19:42:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-let counter = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     counter += 1
+    basic.showNumber(counter)
 })
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
